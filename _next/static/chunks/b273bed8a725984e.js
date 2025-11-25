87d940c974b37c8cf723da1c26099f38a6b29621
Sabrina Yang <sabrina@SabrinadeMacBook-Pro.local>
2025-11-25 22:01:03 +0800

    Fix: Redeploy with cards 220103

---
+++ _next/static/chunks/b273bed8a725984e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/d4d2bd6c09d86c89.js",
+  "static/chunks/turbopack-08798d947697ca77.js"
+])
---
+++ _next/static/chunks/b82ba074d9b64dbb.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/d4d2bd6c09d86c89.js",
+  "static/chunks/turbopack-3d891ca70c2fe006.js"
+])
